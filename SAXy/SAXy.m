2e086f0f6729c870c281d1b35366810d4794cbdf
Richard <richard@Wile-E.local>
2013-02-11 11:35:01 -0800

    Initial Commit

---
+++ SAXy/SAXy.m
@@ -0,0 +1,13 @@
+//
+//  SAXy.m
+//  SAXy
+//
+//  Created by Richard on 2/11/13.
+//  Copyright (c) 2013 Outsource Cafe, Inc. All rights reserved.
+//
+
+#import "SAXy.h"
+
+@implementation SAXy
+
+@end
---
+++ SAXyTests/SAXyTests.m
@@ -0,0 +1,32 @@
+//
+//  SAXyTests.m
+//  SAXyTests
+//
+//  Created by Richard on 2/11/13.
+//  Copyright (c) 2013 Outsource Cafe, Inc. All rights reserved.
+//
+
+#import "SAXyTests.h"
+
+@implementation SAXyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SAXyTests");
+}
+
+@end
